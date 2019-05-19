4b805ae62fe01661006f47a416d995b288d9af88
AtheeshRathnaweera27 <rathnaweeraatheesh72@gmail.com>
2019-05-19 19:35:46 +0530

    Initial commit

---
+++ App.js
@@ -0,0 +1,58 @@
+
+import React, {Component} from 'react';
+import { StyleSheet, Text, View, Image } from 'react-native';  
+import { GiftedChat } from 'react-native-gifted-chat';
+
+
+export default class App extends React.Component {
+
+  state = {
+    messages: [
+      {
+        _id: 1,
+        text: `Hi! I am the FAQ bot 🤖 from Jscrambler.\n\nHow may I help you with today?`,
+        createdAt: new Date(),
+        user: {
+          _id: 2,
+          name: 'FAQ Bot',
+          avatar: 'https://i.imgur.com/7k12EPD.png'
+        }
+      }
+    ]
+  };
+
+  onSend(messages = []) {
+    this.setState(previousState => ({
+      messages: GiftedChat.append(previousState.messages, messages)
+    }));
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>Welcome to React Native!</Text>
+        <Text style={styles.instructions}>To get started, edit App.js</Text>
+        <Text style={styles.instructions}>{instructions}</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
