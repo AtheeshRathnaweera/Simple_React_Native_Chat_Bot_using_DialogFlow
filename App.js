2f79cfe10d25a9c8563b5cec81dcf1680cd8edec
AtheeshRathnaweera27 <rathnaweeraatheesh72@gmail.com>
2019-05-19 21:02:51 +0530

    Final commit

--- App.js
+++ App.js
@@ -1,58 +1,85 @@
 
-import React, {Component} from 'react';
-import { StyleSheet, Text, View, Image } from 'react-native';  
+import React,{Component} from 'react';
+import { StyleSheet, Text, View, Image } from 'react-native';
 import { GiftedChat } from 'react-native-gifted-chat';
 
+import { Dialogflow_V2 } from 'react-native-dialogflow';
 
-export default class App extends React.Component {
+import { dialogflowConfig } from './env';
+
+const BOT_USER = {
+  _id: 2,
+  name: 'FAQ Bot',
+  avatar: 'https://cdn3.iconfinder.com/data/icons/avatars-15/64/_Ninja-2-512.png'
+};
+
+export default class App extends Component {
 
   state = {
     messages: [
       {
         _id: 1,
-        text: `Hi! I am the FAQ bot 🤖 from Jscrambler.\n\nHow may I help you with today?`,
+        text: `Hi! I am the ninja-test bot 🤖.\nHow may I help you with today?`,
         createdAt: new Date(),
-        user: {
-          _id: 2,
-          name: 'FAQ Bot',
-          avatar: 'https://i.imgur.com/7k12EPD.png'
-        }
+        user: BOT_USER
       }
     ]
   };
 
+  componentDidMount() {
+    Dialogflow_V2.setConfiguration(
+      dialogflowConfig.client_email,
+      dialogflowConfig.private_key,
+      Dialogflow_V2.LANG_ENGLISH_US,
+      dialogflowConfig.project_id
+    );
+  }
+
+
+
   onSend(messages = []) {
+
     this.setState(previousState => ({
       messages: GiftedChat.append(previousState.messages, messages)
+    }));
+
+    let message = messages[0].text;
+    Dialogflow_V2.requestQuery(
+      message,
+      result => this.handleGoogleResponse(result),
+      error => console.log(error)
+    );
+  }
+
+  handleGoogleResponse(result) {
+    let text = result.queryResult.fulfillmentMessages[0].text.text[0];
+    this.sendBotResponse(text);
+  }
+
+  sendBotResponse(text) {
+    let msg = {
+      _id: this.state.messages.length + 1,
+      text,
+      createdAt: new Date(),
+      user: BOT_USER
+    };
+
+    this.setState(previousState => ({
+      messages: GiftedChat.append(previousState.messages, [msg])
     }));
   }
 
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+      <View style={{ flex: 1, backgroundColor: '#fff' }}>
+        <GiftedChat
+          messages={this.state.messages}
+          onSend={messages => this.onSend(messages)}
+          user={{
+            _id: 1
+          }}
+        />
       </View>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
---
+++ env.js
@@ -0,0 +1,13 @@
+export const dialogflowConfig = {
+    "type": "service_account",
+    "project_id": "ninja-test-bot",
+    "private_key_id": "37cebf90b0d9ae41db616b28b65f7458763d95c2",
+    "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQDoL5TFJfi3q0RT\nqZmToFvhsYsPvNld26SDbmKJRKm4MoD2qDt0akFMwZsLFFb1lFkPKj/rKx5RtDGm\nCY41B3KoKUJvw8qDi0z/Y+90zh2tksZVyLSDZQpjEgsvDz1VkeDYZCQu3gdhZVvB\nz2FQmHuJYVjDAG/yjHOLPyukFWO/f1fr5VEd68bCrYxqCZPInfsBDMkQ8cTNiTFu\nUUee/RglQOqMH+beiEArb4Qb7OjOYGVjvMbkkurvX9YW8HOk5bTWkr+pfJRweZnE\nrWsvRFREkiLu7fRwB/jg/gPsnlGnl+UrpVQgwsgTfMVizFNkHQsqrrBoKsaBmJfa\n/F7b3wGJAgMBAAECggEATBy45I3HhBH5IoHQzdN9+zSztNN/3+QuAEWxuHTRPAgW\nPYU1hDqGsG4w8IdY4EUikrWTYXhvhnIO6nJscxa/b7+ivzeMDeYetgOhDgW5NdqM\n+LcD8aSwLHzQPLt+rCvXbKgbMl/nRHaxBw3/xSdPK+BE9BuTFwwSnl11ZWL7oVyx\n6bmw5RVhLl/ug6Oo2hu5SHwskcsDe02qSftxRCzmqO5k/ik81h2PLd0x4j/Qcat9\nQIISVH54VGZ8xcrLqJ8d301PXbeC8jHhQpV6wAWg1ArGZmj877kLD4aiE250elJx\nfHVX9gX1yAC7ruew3HdY9G/+v3ug/F+38cgwiZUbjwKBgQD44W72wHk22d7MbtDq\n0yXZnjus0CV1HW7sy6bLV8WUCeyNNvH4+9iJocLaMCWsoAmrfVU50c26FbEEER1m\n50AFjiId/Iz4EPM4toG1Tg1ZeCBnnaJKDA/jbCdQ7qvxn6lEkqhnTPdzbN4dMJXB\nncVGAALw26GHwAwNAt3ozliA2wKBgQDu0+QljjL06dM5ywS/XSscoot1wWhltIKD\n4/CRMg7LbsWD2FBHCadBSj8XkV/Oinyn916DvgHgMpXWEKAlCezLUpQS9LC7xQjT\nUEK6era3YZE9Fn8uSZxhQo5oqI+6hMpKmaIsQigHTDxdWvS0svVzghB3kF5dh4bm\n+USeZEFSawKBgEaV0R3EatxmoCzExRXEerikivIxOl5j0aDxBUYoh4273s4SJkBi\nVQB700KEYzr5TKTJcBFs8nbzTfgETp1vupWYwQ5WITj9va14/YKJN3oHetFIQM3V\na/ApLWFjv3pQuPYMzhw2FPw0hS4sKFz4PQnkWPEsqTxR+Fj8Vil/1FP/AoGBAOwN\n8wpLqoH4OxZ8W4zpgZsj7cPODmm4KOhDe5uHZqgcLI1MWMxd9jrKL+IThiaXKu65\nyWldfG0kkoSqmh8B2FJ+pGy3/ZcaAMccJVg7wi0u+mbuvXX6u95F8ovvT2CDr45c\nm7YzDW1pJKEBLa0M1R3+CFilESBQvD2/UttbLFVRAoGBAL/a0v1sdDNLUCfIZatD\nNfBKxvBR+7g+yBMyP+jJZg4c0UjMhhbiIv8qtCHZs++tcEBeKeviqodOy52e3KxS\neNYmj4K9Oh6UCe9+QwIkKQUPV7nbDoA6YsTBfenpTO0BOVrMMa6hozV6yKzsGcid\nZNqrbbqgLBaRFirbFYwk/5II\n-----END PRIVATE KEY-----\n",
+    "client_email": "dialogflow-sqsvhi@ninja-test-bot.iam.gserviceaccount.com",
+    "client_id": "116736796914201590451",
+    "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+    "token_uri": "https://oauth2.googleapis.com/token",
+    "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+    "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/dialogflow-sqsvhi%40ninja-test-bot.iam.gserviceaccount.com"
+  };
+  
